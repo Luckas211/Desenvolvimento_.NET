295a539201b8a9d239e5d8c394fa42c8722078be
Luckas211 <lucas_rj211@hotmail.com>
2025-11-10 01:27:42 -0300

    atualização das aulas API

---
+++ ModuloAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ModuloAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDataHora()
+        {
+            var obj = new
+            {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToLongTimeString()
+            };
+
+            return Ok(obj);
+        }
+    }
+}
